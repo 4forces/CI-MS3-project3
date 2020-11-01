7f4d5029ecb617cc9b9e25aee2cdf70ca56fd49b
4forces <toh.tingji@gmail.com>
2020-11-01 01:53:24 +0000

    Add secret_key, amend mongo_uri and mongo_dbname

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "your_secret_key_here")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Administrator1@cluster0.expze.mongodb.net/giftkids?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
+os.environ.setdefault("SECRET_KEY", "K6X7i{b1CWkQ97r")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Administrator1@cluster0.expze.mongodb.net/gitkids?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "giftkids")
