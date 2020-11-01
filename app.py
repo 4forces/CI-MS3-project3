09776f7d2d5089f80f55ba1bd15a94ed71851f00
4forces <toh.tingji@gmail.com>
2020-11-01 02:04:20 +0000

    Remove unused code

--- app.py
+++ app.py
@@ -23,22 +23,9 @@ print('DB name:', os.environ.get("MONGO_DBNAME"))
 mongo = PyMongo(app)
 
 
-# # initialise Items dtabase
-# app = Flask(__name__)
-# itemsdb = {}
-# with open('items.json') as fp:
-#     itemsdb = json.load(fp)
-
-
 # Secret key for flash messages
 app = Flask(__name__)
 app.secret_key = b'laksdfoi323d'
-
-
-# # function to save items
-# def save_items():
-#     with open('items.json', 'w') as fp:
-#         json.dump(itemsdb, fp)
 
 
 @app.route('/')  # ok
@@ -74,24 +61,10 @@ def item_list():
 # view item details
 @app.route('/items/<item_id>')  # ok
 def view_item_details(item_id):
-    # print(request.form)
-    # selected_item = None
     item = list(mongo.db.items.find({"_id": ObjectId(item_id)}))
     print('Item selected for VIEW:', item)
-    # for each_item in items:
-    #     if each_item["_id"] == item_id:
-    #         selected_item = each_item
-    #         break
-
-    # if selected_item:
     return render_template('item_details.template.html', item=item)
-    # else:
-    # return render_template('item_notfound.template.html', item_id=item_id)
 
-
-# @app.route('/items/post') # ok
-# def show_post_item():
-#     return render_template('post_item.template.html')
 
 # add item - with ['POST']
 @app.route('/items/post', methods=['POST', 'GET'])  # ok
@@ -131,47 +104,6 @@ def edit_items(item_id):
     item = mongo.db.items.find_one({"_id": ObjectId(item_id)})
     return render_template('edit_item.template.html', item=item)
 
-# def show_edit_items(item_id):
-#     print(request.form)
-#     selected_item = None
-#     for each_item in itemsdb:
-#         if each_item["id"] == item_id:
-#             selected_item = each_item
-#             break
-
-#     if selected_item:
-#         return render_template('edit_item.template.html', item=selected_item)
-
-#     else:
-#         return render_template('item_notfound.template.html',
-#                                item_id=item_id)
-
-
-# @app.route('/items/<item_id>/edit', methods=['POST'])
-# def process_edit_item(item_id):
-#     print(request.form)
-#     selected_item = None
-#     for each_item in itemsdb:
-#         if each_item["id"] == item_id:
-#             selected_item = each_item
-#             break
-
-#     if selected_item:
-#         selected_item["name"] = request.form.get('item_name')
-#         selected_item["description"] = request.form.get('description')
-#         selected_item["age"] = request.form.get('age')
-#         selected_item["condition"] = request.form.get('condition')
-#         selected_item["delete"] = request.form.get('delete_after')
-#         save_items()
-#         flash(
-#             f"Item {selected_item['name']}"
-#             f"  has been edited successfully.")
-#         return redirect(url_for('item_list'))
-
-#     else:
-#         return render_template('item_notfound.template.html',
-#                                item_id=item_id)
-
 
 # delete item - with ['POST']
 @app.route("/items/<item_id>/delete", methods=["GET", "POST"])  # ok
@@ -185,40 +117,6 @@ def delete_items(item_id):
     return render_template('show_delete_item.template.html',
                            item=item)
 
-
-# @app.route('/items/<int:item_id>/delete')
-# def show_delete_items(item_id):
-#     print(request.form)
-#     selected_item = None
-#     for each_item in itemsdb:
-#         if each_item["id"] == item_id:
-#             selected_item = each_item
-#             break
-
-#     if selected_item:
-#         return render_template('show_delete_item.template.html',
-#                                item=selected_item)
-
-#     else:
-#         return render_template('item_notfound.template.html',
-#                                item_id=item_id)
-
-
-# @app.route('/items/<int:item_id>/delete', methods=['POST'])
-# def process_show_delete_item(item_id):
-#     selected_item = None
-#     for each_item in itemsdb:
-#         if each_item['id'] == item_id:
-#             selected_item = each_item['id']
-#             break
-
-#     if selected_item:
-#         itemsdb.remove(each_item)
-#         save_items()
-#         return redirect(url_for('item_list'))
-
-#     else:
-#         return render_template('item_notfound.template.html', item_id=item_id)
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
