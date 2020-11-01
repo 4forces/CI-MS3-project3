ce9078f21ec3a26faeeceb3ede20c9b3c07680a4
4forces <toh.tingji@gmail.com>
2020-11-01 02:29:41 +0000

    Amend mongo_uri to giftkids (note gitkids...)

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "K6X7i{b1CWkQ97r")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Administrator1@cluster0.expze.mongodb.net/gitkids?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Administrator1@cluster0.expze.mongodb.net/giftkids?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "giftkids")
