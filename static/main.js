7dae3fdf6983434ac604d4fca72a33085f94cfb9
4forces <toh.tingji@gmail.com>
2020-11-02 04:13:14 +0000

    Add fxn in main.js to change text to fontawesome for 'edit' n 'delete'

--- static/main.js
+++ static/main.js
@@ -10,3 +10,19 @@ function getDate() {
   let dateSubStr = Date().substring(4,15)
   document.getElementById('date').value = dateSubStr;
 }
+
+function changeX() {
+    document.getElementById('delete').innerHTML = '<i id="fa-cross" class="fa fa-remove"></i>';
+}
+
+function changeDelete() {
+    document.getElementById('fa-cross').innerHTML = '<span id="delete">Delete</span>';
+}
+
+function changePencil() {
+    document.getElementById('edit').innerHTML = '<i id="fa-pencil" class="fa fa-pencil"></i>';
+}
+
+function changeEdit() {
+    document.getElementById('fa-pencil').innerHTML = '<span id="edit">Edit</span>';
+}
