39257b80beee0634099a04039d8152fe59dab342
4forces <toh.tingji@gmail.com>
2020-11-18 07:10:33 +0000

    Organise and label code

--- static/main.js
+++ static/main.js
@@ -1,43 +1,5 @@
-// Upload photo
-// document.querySelector('.custom-file-input').addEventListener('change',function(e){
-//   var fileName = document.getElementById("myInput").files[0].name;
-//   var nextSibling = e.target.nextElementSibling
-//   nextSibling.innerText = fileName
-// })
-
-
-
-
-// $(function() {
-//     toastr.options = {
-//     "debug": false,
-//     "positionClass": "toast-bottom-full-width",
-//     "onclick": null,
-//     "fadeIn": 300,
-//     "fadeOut": 1000,
-//     "timeOut": 5000,
-//     "extendedTimeOut": 1000
-//     }
-// })
-
-// Get current date
+// Get current date on submit item
 function getDate() {
   let dateSubStr = Date().substring(4,15) + ', ' + Date().substring(16,24) ;
   document.getElementById('date').value = dateSubStr;
-}
-
-function changeX() {
-    document.getElementById('delete').innerHTML = '<i id="fa-cross" class="fa fa-remove"></i>';
-}
-
-function changeDelete() {
-    document.getElementById('fa-cross').innerHTML = '<span id="delete">Delete</span>';
-}
-
-function changePencil() {
-    document.getElementById('edit').innerHTML = '<i id="fa-pencil" class="fa fa-pencil"></i>';
-}
-
-function changeEdit() {
-    document.getElementById('fa-pencil').innerHTML = '<span id="edit">Edit</span>';
 }
